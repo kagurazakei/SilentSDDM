bec13b6a91f9d4880ffa31d17fbdb1c0834d1821
kagurazakei <maotsugiri@gmail.com>
2026-02-08 11:47:47 +0630

    🚀 update(cursor): change default cursor themes

--- nix/module.nix
+++ nix/module.nix
@@ -2,13 +2,38 @@
   lib,
   pkgs,
   config,
+  inputs,
   ...
-}: let
-  inherit (lib) map pipe flatten flip elem assertMsg;
-  inherit (lib) mkIf mkEnableOption mkOption literalExpression;
-  inherit (lib.types) enum attrsWith path attrs package;
+}:
+let
+  inherit (lib)
+    map
+    pipe
+    flatten
+    flip
+    elem
+    assertMsg
+    ;
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    mkOption
+    literalExpression
+    ;
+  inherit (lib.types)
+    enum
+    attrsWith
+    path
+    attrs
+    package
+    ;
   inherit (lib.strings) removeSuffix;
-  inherit (lib.attrsets) attrNames attrValues mapAttrs filterAttrs;
+  inherit (lib.attrsets)
+    attrNames
+    attrValues
+    mapAttrs
+    filterAttrs
+    ;
   inherit (lib.filesystem) listFilesRecursive;
 
   users = attrNames config.users.users;
@@ -19,10 +44,11 @@
 
   # TODO pass gitRev somehow? (maybe like quickshell?)
   # this is done this way to be independent of the flake
-  silent = pkgs.callPackage ./package.nix {};
+  silent = pkgs.callPackage ./package.nix { };
   cfg = config.programs.silentSDDM;
   silent' = cfg.package'; # silent with configuration applied
-in {
+in
+{
   options.programs.silentSDDM = {
     enable = mkEnableOption "silentSDDM theme";
 
@@ -44,7 +70,7 @@ in {
         placeholder = "image";
         elemType = path;
       };
-      default = {};
+      default = { };
       example = literalExpression ''
         {
           reze = pkgs.fetchurl {
@@ -64,7 +90,7 @@ in {
         placeholder = "image";
         elemType = path;
       };
-      default = {};
+      default = { };
       example = literalExpression ''
         {
           # <username> = <path / drv>
@@ -80,7 +106,7 @@ in {
 
     settings = mkOption {
       type = attrs;
-      default = {};
+      default = { };
       example = literalExpression ''
         {
             "LoginScreen.LoginArea.Avatar" = {
@@ -120,11 +146,16 @@ in {
       visible = false;
       default = cfg.profileIcons;
       apply = flip pipe [
-        (filterAttrs (user: _: assertMsg (elem user users) "programs.silentSDDM.profileIcons: '${user}' is not a valid user"))
-        (mapAttrs (user: icon: [
-          "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
-          "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${icon}"
-        ]))
+        (filterAttrs (
+          user: _:
+          assertMsg (elem user users) "programs.silentSDDM.profileIcons: '${user}' is not a valid user"
+        ))
+        (mapAttrs (
+          user: icon: [
+            "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
+            "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${icon}"
+          ]
+        ))
         attrValues
         flatten
       ];
@@ -133,11 +164,15 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [silent' silent'.test];
+    environment.systemPackages = [
+      silent'
+      silent'.test
+      inputs.shizuruPkgs.packages.${pkgs.stdenv.hostPlatform.system}.kureiji-ollie-cursors
+    ];
     qt.enable = true;
     systemd.services.display-manager.enable = true;
     services.displayManager.sddm = {
-      wayland.enable = ! config.services.xserver.enable;
+      wayland.enable = !config.services.xserver.enable;
       enable = true;
       package = lib.mkDefault pkgs.kdePackages.sddm;
       theme = "silent";
@@ -149,10 +184,13 @@ in {
           GreeterEnvironment = "QML2_IMPORT_PATH=${silent'}/share/sddm/themes/silent/components/,QT_IM_MODULE=qtvirtualkeyboard";
           InputMethod = "qtvirtualkeyboard";
         };
+        Theme = {
+          CursorTheme = "Kureiji-Ollie-v2";
+        };
       };
     };
 
     # setup profile pictures
-    systemd.tmpfiles.rules = mkIf (cfg.profileIcons != {}) cfg.profileIcons';
+    systemd.tmpfiles.rules = mkIf (cfg.profileIcons != { }) cfg.profileIcons';
   };
 }
